81b22a933a74d517c028aea5d997b20c19fec9ee
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-07-18 03:54:58 +0800

    feat: demo 简单实现清单查看页面

--- IYaManifestDemo/Pages/ManifestViewerPage.xaml.cs
+++ IYaManifestDemo/Pages/ManifestViewerPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common_Util.Log;
+using IYaManifestDemo.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,13 @@ namespace IYaManifestDemo.Pages
         public ManifestViewerPage()
         {
             InitializeComponent();
+
+            ViewModel.OperationLogger = LevelLoggerHelper.LogTo(LogShower, "操作", string.Empty);
+            ViewModel.TrackLogger = LevelLoggerHelper.LogTo(LogShower, "执行过程", string.Empty);
+            DataContext = ViewModel;
         }
+
+        public ManifestViewerPageViewModel ViewModel { get; private set; } = new();
     }
+
 }
