d5b80f527ef9493efa10b58a8043a1b5867bbb03
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-07-18 03:54:29 +0800

    feat: V1版本的清单项添加属性 Brief, 以实现 IBriefInfo 接口
    此属性在生成 XML 格式的清单项时不输出

--- IYaManifest/Core/V1/ManifestItem.cs
+++ IYaManifest/Core/V1/ManifestItem.cs
@@ -1,5 +1,6 @@
 ﻿using Common_Util.Attributes.Xml;
 using Common_Util.Data.Structure.Value;
+using Common_Util.Module;
 using IYaManifest.Enums;
 using IYaManifest.Interfaces;
 using System;
@@ -15,7 +16,7 @@ namespace IYaManifest.Core.V1
     /// 默认实现的清单资源项
     /// <para>外部资源使用与清单文件的相对坐标表示, 外部资源无需校验 MD5 !</para>
     /// </summary>
-    public class ManifestItem : IManifestItem
+    public class ManifestItem : IManifestItem, IBriefInfo
     {
         [XmlTextValue]
         public string AssetId { get => AssetCode; set => AssetCode = value; }
@@ -40,10 +41,10 @@ namespace IYaManifest.Core.V1
         public AssetDataStorageModeEnum StorageMode { get; set; }
 
         [XmlElement]
-        public byte[] MD5 
+        public byte[] MD5
         {
             get => FixedMD5 ?? md5;
-            set => md5 = value ?? []; 
+            set => md5 = value ?? [];
         }
         private byte[] md5 = [];
         /// <summary>
@@ -55,9 +56,38 @@ namespace IYaManifest.Core.V1
         public long LocationStart { get; set; }
         public long LocationLength { get; set; }
 
+        /// <summary>
+        /// 资源项的备注
+        /// </summary>
+        public string? Remark { get; set; }
 
         [XmlTextValue]
         public string? OutsidePath { get; set; }
+
+        /// <summary>
+        /// 简述信息
+        /// </summary>
+        [XmlIgnore]
+        public string Brief 
+        {
+            get
+            {
+                StringBuilder sb = new($"[{AssetId}]({AssetType})_{StorageMode}_");
+                switch (StorageMode)
+                {
+                    case AssetDataStorageModeEnum.InManifest:
+                        sb.Append("内嵌资源");
+                        break;
+                    case AssetDataStorageModeEnum.ManifestData:
+                        sb.Append($"数据区域_S:{LocationStart}_L:{LocationLength}");
+                        break;
+                    case AssetDataStorageModeEnum.Outside:
+                        sb.Append($"相对路径_{OutsidePath}");
+                        break;
+                }
+                return sb.ToString();
+            }
+        }
 
         #region 序列号常量参数
         public const string INNER_DATA_TAG_NAME = "Data";
