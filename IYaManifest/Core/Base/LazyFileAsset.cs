8aa73fcfe155224c66720fc3ca95f27f21ae6ed3
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-07-18 03:54:47 +0800

    feat: 整理懒加载资源的接口, 调整 LazyFileAsset 资源的实现
    最主要的改动:
    1. 修改方法名
    2. 默认情况下, 调用 Asset 属性的 get 不再自动加载资源, 但是可以配置 get 时, 如果未加载就加载

--- IYaManifest/Core/Base/LazyFileAsset.cs
+++ IYaManifest/Core/Base/LazyFileAsset.cs
@@ -19,12 +19,14 @@ namespace IYaManifest.Core.Base
         /// 目标资源类
         /// </summary>
         public required Type TargetAssetClass { get; set; }
+
+        public override Type ExpectReferenceType => TargetAssetClass;
     }
     /// <summary>
-    /// 数据存储在文件中的懒加载资源, 其 <see cref="Dispose"/> 方法仅调用 <see cref="Clear"/> 方法, 以方便使用
+    /// 数据存储在文件中的懒加载资源, 其 <see cref="Dispose"/> 方法仅调用 <see cref="Unload"/> 方法, 以方便使用
     /// </summary>
     /// <typeparam name="TAsset"></typeparam>
-    public class LazyFileAsset<TAsset> : IAsset, IDisposable
+    public class LazyFileAsset<TAsset> : ILazyAsset<TAsset>, IDisposable
         where TAsset : IAsset
     {
         
@@ -49,6 +51,12 @@ namespace IYaManifest.Core.Base
         /// </summary>
         public required IAssetWriteReadImpl ReadImpl { get; set; }
 
+        IAsset? ILazyAsset.Asset { get => Asset; }
+
+        /// <summary>
+        /// 未加载状态下, 获取属性 <see cref="Asset"/> 时, 是否自动调用加载
+        /// </summary>
+        public bool AutoLoadAsset { get; set; } = false;
         /// <summary>
         /// 资源, 未加载状态下调用会从文件中读取数据并加载为对象
         /// </summary>
@@ -56,33 +64,9 @@ namespace IYaManifest.Core.Base
         {
             get
             {
-                if (!loaded)
+                if (AutoLoadAsset && !loaded)
                 {
-                    lock (locker)
-                    {
-                        if (!loaded)
-                        {
-                            using var fs = File.OpenRead(FileName);
-                            using OffsetWrapperStream ows = new OffsetWrapperStream(fs, Start, Length);
-                            var result = ReadImpl.LoadFrom(ows);
-                            if (result.IsSuccess)
-                            {
-                                if (result.Data == null)
-                                {
-                                    throw new OperationFailureException((OperationResult)$"加载方法执行成功, 但是结果中的数据值为 null");
-                                }
-                                _asset = (TAsset)result.Data;
-                                _asset.AssetId = AssetId;
-                                OnLoad?.Invoke(this, _asset);
-                            }
-                            else
-                            {
-                                throw new OperationFailureException(result);
-                            }
-                        }
-                        loaded = true;
-                        
-                    }
+                    Load();
                 }
                 return _asset;
             }
@@ -101,11 +85,54 @@ namespace IYaManifest.Core.Base
         /// </summary>
         public bool Loaded => loaded;
 
+        public virtual Type ExpectReferenceType => typeof(TAsset);
+
+
+        /// <summary>
+        /// 调用 <see cref="Unload"/>
+        /// </summary>
+        public void Dispose()
+        {
+            Unload();
+        }
+
+        #region 加卸载操作
+        public void Load()
+        {
+            lock (locker)
+            {
+                if (!loaded)
+                {
+                    using var fs = File.OpenRead(FileName);
+                    using OffsetWrapperStream ows = new OffsetWrapperStream(fs, Start, Length);
+                    var result = ReadImpl.LoadFrom(ows);
+                    if (result.IsSuccess)
+                    {
+                        if (result.Data == null)
+                        {
+                            throw new OperationFailureException((OperationResult)$"加载方法执行成功, 但是结果中的数据值为 null");
+                        }
+                        _asset = (TAsset)result.Data;
+                        _asset.AssetId = AssetId;
+                        OnLoad?.Invoke(this, _asset);
+                    }
+                    else
+                    {
+                        throw new OperationFailureException(result);
+                    }
+
+                    loaded = true;
+                    LoadedStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+
+            }
+        }
 
         /// <summary>
         /// 清理当前已加载的资源. 线程不安全, 如果在资源加载过程中调用, 可能会导致错误
         /// </summary>
-        public void Clear()
+        /// <exception cref="NotImplementedException"></exception>
+        public void Unload()
         {
             lock (locker)
             {
@@ -113,18 +140,13 @@ namespace IYaManifest.Core.Base
                 if (loaded)
                 {
                     OnUnload?.Invoke(this);
+                    loaded = false;
+                    LoadedStateChanged?.Invoke(this, EventArgs.Empty);
                 }
-                loaded = false;
             }
         }
 
-        /// <summary>
-        /// 调用 <see cref="Clear"/>
-        /// </summary>
-        public void Dispose()
-        {
-            Clear();
-        }
+        #endregion
 
         #region 事件
         /// <summary>
@@ -137,6 +159,8 @@ namespace IYaManifest.Core.Base
         /// <para>在方法内不能调用加载资源的方法, 否则将产生互锁</para>
         /// </summary>
         public event Action<LazyFileAsset<TAsset>>? OnUnload;
+
+        public event EventHandler? LoadedStateChanged;
         #endregion
     }
 }
---
+++ IYaManifest/Interfaces/ILazyAsset.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IYaManifest.Interfaces
+{
+    /// <summary>
+    /// 懒加载资源接口
+    /// </summary>
+    public interface ILazyAsset : IAsset
+    {
+        /// <summary>
+        /// 以懒加载模式管理的资源
+        /// </summary>
+        public IAsset? Asset { get; }
+
+        /// <summary>
+        /// 加载资源
+        /// </summary>
+        public void Load();
+        /// <summary>
+        /// 卸载已加载的资源
+        /// </summary>
+        public void Unload();
+
+
+        /// <summary>
+        /// 资源是否已经载入
+        /// </summary>
+        public bool Loaded { get; }
+
+        /// <summary>
+        /// 预期引用类型, 即加载完成后, <see cref="Asset"/> 对象的类型
+        /// </summary>
+        public Type? ExpectReferenceType { get; }
+
+        /// <summary>
+        /// 是否已载入状态变更事件
+        /// </summary>
+        public event EventHandler LoadedStateChanged;
+    }
+    public interface ILazyAsset<TAsset> : ILazyAsset
+        where TAsset : IAsset
+    {
+        /// <summary>
+        /// 以懒加载模式管理的资源
+        /// </summary>
+        public new TAsset? Asset { get; }
+    }
+}
